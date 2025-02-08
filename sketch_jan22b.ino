8ec29827de86023d2b4c2e3076584099a7aea9c1
Chris <inonikis3gs@gmail.com>
2025-02-08 21:40:20 +0200

    Added comments

--- sketch_jan22b.ino
+++ sketch_jan22b.ino
@@ -1,11 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A0, INPUT);
-  Serial.begin(9600);
+  pinMode(A0, INPUT); //sets the pin A0 to recieve data
+  Serial.begin(9600); //initialises the serial interface at a baud rate of 9600bps
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(A0));
-  delay(5000);
+  Serial.println(analogRead(A0)); //prints the voltage of A0
+  delay(5000); //waits 5seconds
 }
